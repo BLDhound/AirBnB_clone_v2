51746b64f63687453ed546e0e7ecff02f38d9c70
BLDhound <lekarrikaila567@gmail.com>
2023-04-22 11:52:18 +0000

    added bin bash

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+"""module hello route of flask"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello():
-    return "Hello HBNB!"
+    """Print first message"""
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python3
+"""module hello route of flask"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
 
-@app.route('/hbnb', methods=['GET'], strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Print first message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Print route hbnb message"""
+    return 'HBNB'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
